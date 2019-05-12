a1da9690dae66d7ab3a379c50ecb36f9c76320eb
Steven Balerna <stevenbalerna@stevens-air.attlocal.net>
2019-05-12 15:26:46 -0400

    1

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// General Variables
+
+var random_result;
+var wins;
+var lost;
+
+for (var i = 0; i < 4; i ++){
+    console.log("hey");
+}
+//Infinity Stone Variables
+
+
+// Score Variables
+
+
+// Wins and Losses Variables
+
+// Gameplay Functions
+
+$("#power").click(function() {
+    alert("lit");
+});
+
+$("#reality").click(function() {
+    alert("hey");
+});
+
+$("#time").click(function() {
+    alert("day");
+});
+
+$("#soul").click(function() {
+    alert("tee");
+});
